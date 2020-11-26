9a94367a0c6bf51e033de1414ec4acc98ef95e78
elprimo535180046 <70675036+elprimo535180046@users.noreply.github.com>
2020-11-26 09:30:06 +0700

    Added mongoDB (partially)
    Still some WIP

---
+++ UAS/models/bookSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+mongoose.connect(
+    "mongodb://127.0.0.1:27017/db-dugongbookstore.books",
+    { useNewUrlParser: true}
+);
+const db = mongoose.connection;
+
+db.once("open",()=> {
+    console.log("Connection established using Mongoose.");
+});
+//Type 1
+const bookSchema = mongoose.Schema({
+    judul: String,
+    author: String,
+    year: String,
+    isbn: String,
+    pos: String,
+    stock: Number,
+    cover: String,
+    sinopsis: String,
+    loc: String
+});
+
+const Book = mongoose.model("Book",bookSchema);
+
+module.exports = Book;
--- UAS/routes/index.js
+++ UAS/routes/index.js
@@ -1,4 +1,6 @@
-const express = require('express');
+const express = require("express");
+
+const User = require('../models/emailSchema');
 
 const router = express.Router();
 
@@ -6,5 +8,27 @@ router.get('/',async(req,res)=>{
     //Show to main menu
     res.render('pages/index');
 });
+
+//Post still error, not detected.
+router.post('/index',async(req,res)=>{
+    const input = req.body.email;
+
+    User.find({email: input}).exec((err,data)=>{
+        if (data) console.log("Email ditemukan");
+        else{
+            console.log("Email tidak ditemukan. BISA DI PUSH");
+        }
+    });
+/* 
+    User.insertOne({email:input},(err,raw)=>{
+        User.find({email:input}).exec((err,data)=>{
+            if (data) console.log("No inputed email in database.");
+            else {
+                console.log("Email sudah terdaftar");
+            }
+        });
+    }); */
+});
+
 
 module.exports = router;
