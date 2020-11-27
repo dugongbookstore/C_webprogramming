22620781e888349b249b4fdfda08537bbbe95d39
elprimo535180046 <70675036+elprimo535180046@users.noreply.github.com>
2020-11-27 13:36:11 +0700

    Added function to push email to DB.
    Still need to add FIND function so no email is duplicate.

--- UAS/routes/index.js
+++ UAS/routes/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
-const User = require('../models/emailSchema');
+var MongoClient = require('mongodb').MongoClient,
+    dbURL = "mongodb://127.0.0.1:27017",
+    dbName = "db-dugongbookstore"
 
 const router = express.Router();
 
@@ -9,26 +11,25 @@ router.get('/',async(req,res)=>{
     res.render('pages/index');
 });
 
-//Post still error, not detected.
-router.post('/index',async(req,res)=>{
-    const input = req.body.email;
-
-    User.find({email: input}).exec((err,data)=>{
-        if (data) console.log("Email ditemukan");
-        else{
-            console.log("Email tidak ditemukan. BISA DI PUSH");
+//Need FIND function, then push to DB if not found.
+//Currently, push is OK.
+router.post('/email', (req,res)=>{
+    //Get email
+    const newsLetter = req.body.newsLetter;
+    //Connect to DB
+    MongoClient.connect(dbURL, function(err,client){
+        if (err){
+            throw err;
         }
+        //Function to push ONE DATA
+        let db = client.db(dbName);
+        db.collection("emails")
+        .insertOne({ email: newsLetter},(err,db)=>{
+            if (err) throw err;
+            res.render('pages/index');
+        })
+        console.log("Email registered on DB."); //Confirmation if OK to push
     });
-/* 
-    User.insertOne({email:input},(err,raw)=>{
-        User.find({email:input}).exec((err,data)=>{
-            if (data) console.log("No inputed email in database.");
-            else {
-                console.log("Email sudah terdaftar");
-            }
-        });
-    }); */
 });
-
 
 module.exports = router;
