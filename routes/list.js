57c5999ad8c9416d98d9f989c1140e14688641c1
elprimo535180046 <70675036+elprimo535180046@users.noreply.github.com>
2020-12-13 12:54:44 +0700

    Added force login, rewrite recommended, added un recommend (need better name), added edit function

--- routes/list.js
+++ routes/list.js
@@ -7,28 +7,44 @@ var MongoClient = require('mongodb').MongoClient,
 
 const router = express.Router();
 var bookModel = require('../models/bookSchema');
+var mongoose = require('mongoose');
+const upload = require('./upload');
+var Book = mongoose.model('Book');
 
 router.get('/',async(req,res)=>{
-    MongoClient.connect(dbURL, function(err,client){
-        if (err){
-            throw err;
-        }
-        //Function to read ALL DATA
-        let db = client.db(dbName);
-        db.collection("book").find().toArray((err,data)=>{
-            if (err) throw err;
-            res.render('pages/list', {book: data})
-        })
-    });
+    if(!req.session.user) {
+        res.redirect('/me7rhg/login')
+    }
+    else {
+        MongoClient.connect(dbURL, function(err,client){
+            if (err){
+                throw err;
+            }
+            //Function to read ALL DATA
+            let db = client.db(dbName);
+            db.collection("book").find().toArray((err,data)=>{
+                if (err) throw err;
+                res.render('pages/list', {book: data})
+            })
+        });
+    }
 });
 //Recommend
 router.post('/rec/:isbn', async(req,res)=>{
     const isbn = req.params.isbn;
-    console.log(isbn);
     bookModel.updateOne({ISBN: isbn},{rec: true},(err,raw)=>{
         bookModel.find({ISBN: isbn}).limit(1).exec((err,data)=>{
             if (err) throw err;
-            console.log("Updated.");
+            res.redirect('/list');
+        })
+    })
+})
+//Not Recommend
+router.post('/unrec/:isbn', async(req,res)=>{
+    const isbn = req.params.isbn;
+    bookModel.updateOne({ISBN: isbn},{rec: false},(err,raw)=>{
+        bookModel.find({ISBN: isbn}).limit(1).exec((err,data)=>{
+            if (err) throw err;
             res.redirect('/list');
         })
     })
@@ -36,7 +52,6 @@ router.post('/rec/:isbn', async(req,res)=>{
 //Delete
 router.post('/delete/:isbn', async(req,res) => {
     const isbn = req.params.isbn;
-    console.log(isbn);
     MongoClient.connect(dbURL, function(err,client){
         if (err){
             throw err;
@@ -48,44 +63,54 @@ router.post('/delete/:isbn', async(req,res) => {
         db.collection("book").find({ISBN: isbn}).toArray((err,arr)=>{
             if (err) throw err;
             else if (arr.length === 0){
-                console.log("No book.");
+                console.log("404 - No book.");
             } else {
-                console.log(isbn);
                 db.collection("book").deleteOne({ ISBN: isbn},(err,data)=>{
                     if (err) throw err;
                     res.redirect('/list');
                 })
-                console.log("Deleted."); 
             }
         });
     });
 });
 
-// router.post('/edit/:ISBN', async(req,res) => {
-//     const isbn = req.params.ISBN;
-//     console.log(isbn);
-//     MongoClient.connect(dbURL, function(err,client){
-//         if (err){
-//             throw err;
-//         }
-        
-//         let db = client.db(dbName);
-
-//         //Function to FIND DATA
-//         db.collection("book").find({ISBN: isbn}).toArray((err,arr)=>{
-//             if (err) throw err;
-//             else if (arr.length === 0){
-//                 console.log("No book.");
-//             } else {
-//                 db.collection("book").find({ ISBN: isbn},(err,data)=>{
-//                     if (err) throw err;
-//                     res.redirect('/edit/:ISBN');
-//                 })
-//                 // res.render('pages/index',{ error: "Email yang diinput sudah terdaftar!"} );
-//                 // console.log("Edit"); //Confirmation if NOT OK to push
-//             }
-//         });
-//     });
-// });
+//Edit - get ISBN
+router.get('/edit/:isbn', async(req,res)=>{
+    const isbn = req.params.isbn;
+    bookModel.find({ISBN: isbn}).limit(1).exec((err,data)=>{
+        if (err) throw err;
+        res.render('pages/edit',{book: data});
+    });
+});
+//Edit - Post update
+router.post('/update',  function(req,res){
+    //Function upload dari multer, bukan dari mongoose. Harus dipake kalo mau upload
+    //gambar.
+    upload(req,res,(error)=>{
+        if(error){
+            console.log(error);
+        }
+        else{
+            var fullpath = req.file.filename;
+            if (!req.file.filename.includes("img/")) {
+                var fullpath = "img/"+req.file.filename;
+            }
+            else {
+                fullpath = req.file.filename;
+            } 
+            Book.updateOne({ISBN: req.body.ISBN},
+                {Status: req.body.status, Type: req.body.type,
+                Judul: req.body.name, Author: req.body.author,
+            Tahun: req.body.year, ISBN: req.body.ISBN,
+            Pos: req.body.lokasi, Stok: req.body.stok,
+            cover: fullpath, sinopsis: req.body.sinopsis,
+            rec: false},(err,raw)=>{
+                Book.find({ISBN: req.body.ISBN}).limit(1).exec((err,data)=>{
+                    if (data) res.redirect('/list');
+                });
+            });
+        }
+    }) 
+});
 
 module.exports = router;
